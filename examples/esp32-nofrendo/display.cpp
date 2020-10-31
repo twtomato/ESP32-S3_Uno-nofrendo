cc20be4761572c33c34beb0addaa1ba69ca74a9d
moononournation <moononournation@gmail.com>
2020-10-31 22:00:17 +0800

    display verbose error

--- examples/esp32-nofrendo/display.cpp
+++ examples/esp32-nofrendo/display.cpp
@@ -40,8 +40,8 @@ Arduino_ST7789 *gfx = new Arduino_ST7789(bus, -1 /* RST */, 1 /* rotation */, tr
 
 /* ST7796 on breadboard */
 // #define TFT_BL 32
-Arduino_ESP32SPI_DMA *bus = new Arduino_ESP32SPI_DMA(12 /* DC */, 5 /* CS */, 18 /* SCK */, 23 /* MOSI */, -1 /* MISO */);
-Arduino_ST7796 *gfx = new Arduino_ST7796(bus, -1 /* RST */, 3 /* rotation */);
+Arduino_DataBus *bus = new Arduino_ESP32SPI_DMA(32 /* DC */, -1 /* CS */, 25 /* SCK */, 33 /* MOSI */, -1 /* MISO */);
+Arduino_TFT *gfx = new Arduino_ST7796(bus, -1 /* RST */, 1 /* rotation */);
 
 /* ST7796 on LCDKit */
 // #define TFT_BL 23
@@ -51,21 +51,11 @@ Arduino_ST7796 *gfx = new Arduino_ST7796(bus, -1 /* RST */, 3 /* rotation */);
 #endif /* custom hardware */
 
 static int16_t w, h, frame_x, frame_y, frame_x_offset, frame_width, frame_height, frame_line_pixels;
-static int16_t bg_color;
+extern int16_t bg_color;
 extern uint16_t myPalette[];
 
 extern "C" void display_init()
 {
-    gfx->begin();
-    bg_color = gfx->color565(24, 28, 24); // DARK DARK GREY
-    gfx->fillScreen(bg_color);
-
-#ifdef TFT_BL
-    ledcAttachPin(TFT_BL, 1);     // assign TFT_BL pin to channel 1
-    ledcSetup(1, 12000, 8);       // 12 kHz PWM, 8-bit resolution
-    ledcWrite(1, TFT_BRIGHTNESS); // brightness 0 - 255
-#endif
-
     w = gfx->width();
     h = gfx->height();
     if (w < 480) // assume only 240x240 or 320x240
@@ -118,15 +108,15 @@ extern "C" void display_write_frame(const uint8_t *data[])
 
         // scale up height x 1.5 have 2 options
         // Option 1: repeat a line for every 2 lines
-        gfx->writeAddrWindow(frame_x, frame_y, frame_width, frame_height);
-        for (int16_t i = 10; i < (10 + 214); i++)
-        {
-            gfx->writeIndexedPixelsDouble((uint8_t *)(data[i] + 8), myPalette, frame_line_pixels);
-            if ((i % 2) == 1)
-            {
-                gfx->writeIndexedPixelsDouble((uint8_t *)(data[i] + 8), myPalette, frame_line_pixels);
-            }
-        }
+        // gfx->writeAddrWindow(frame_x, frame_y, frame_width, frame_height);
+        // for (int16_t i = 10; i < (10 + 214); i++)
+        // {
+        //     gfx->writeIndexedPixelsDouble((uint8_t *)(data[i] + 8), myPalette, frame_line_pixels);
+        //     if ((i % 2) == 1)
+        //     {
+        //         gfx->writeIndexedPixelsDouble((uint8_t *)(data[i] + 8), myPalette, frame_line_pixels);
+        //     }
+        // }
         // Option 2: simply blank a line for every 2 lines
         int16_t y = 0;
         for (int16_t i = 10; i < (10 + 214); i++)
--- examples/esp32-nofrendo/esp32-nofrendo.ino
+++ examples/esp32-nofrendo/esp32-nofrendo.ino
@@ -1,7 +1,13 @@
+/* Arduino Nofrendo
+ * Please check hw_config.h and display.cpp for configuration details
+ */
 #include <esp_wifi.h>
 #include <esp_task_wdt.h>
 #include <SD.h>
+#include <SD_MMC.h>
 #include <SPIFFS.h>
+
+#include <Arduino_GFX_Library.h>
 
 #include "hw_config.h"
 
@@ -9,6 +15,9 @@ extern "C"
 {
 #include <nofrendo.h>
 }
+
+int16_t bg_color;
+extern Arduino_TFT *gfx;
 
 void setup()
 {
@@ -21,21 +30,32 @@ void setup()
     TaskHandle_t idle_0 = xTaskGetIdleTaskHandleForCPU(0);
     esp_task_wdt_delete(idle_0);
 
-    // uncomment one for selecting files stored in SPIFFS or SD
-    SPIFFS.begin(false, FSROOT);
-    FS fs = SPIFFS;
-    // SD.begin(SS, SPI, 40000000, FSROOT);
+    // init display
+    gfx->begin();
+    bg_color = gfx->color565(24, 28, 24); // DARK DARK GREY
+    gfx->fillScreen(bg_color);
+
+#ifdef TFT_BL
+    // turn display backlight on
+    ledcAttachPin(TFT_BL, 1);     // assign TFT_BL pin to channel 1
+    ledcSetup(1, 12000, 8);       // 12 kHz PWM, 8-bit resolution
+    ledcWrite(1, TFT_BRIGHTNESS); // brightness 0 - 255
+#endif
+
+    // filesystem defined in hw_config.h
+    FILESYSTEM_BEGIN
 
     // find first rom file (*.nes)
-    File root = fs.open("/");
+    File root = filesystem.open("/");
+    char *argv[1];
     if (!root)
     {
-        Serial.println("Failed to open directory");
+        Serial.println("Filesystem mount failed! Please check hw_config.h settings.");
+        gfx->println("Filesystem mount failed! Please check hw_config.h settings.");
     }
     else
     {
         bool foundRom = false;
-        char *argv[1];
 
         File file = root.openNextFile();
         while (file)
@@ -62,16 +82,15 @@ void setup()
             file = root.openNextFile();
         }
 
-        if (foundRom)
-        {
-            Serial.println("NoFrendo start!\n");
-            nofrendo_main(1, argv);
-            Serial.println("NoFrendo end!\n");
-        }
-        else
+        if (!foundRom)
         {
             Serial.println("Failed to find rom file, please copy rom file to data folder and upload with \"ESP32 Sketch Data Upload\"");
+            argv[0] = "/";
         }
+
+        Serial.println("NoFrendo start!\n");
+        nofrendo_main(1, argv);
+        Serial.println("NoFrendo end!\n");
     }
 }
 
